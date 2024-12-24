c783bac21c9347a55fe936faea611d697ca81663
Shivam <sg641818@gmail.com>
2024-12-24 11:08:22 +0530

    conneted mongoDB database

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,34 @@
+
+import mongoose from 'mongoose'
+
+
+type ConnectionObject = {
+    isConneted ? : number
+}
+
+const connetion : ConnectionObject = {}
+
+
+async function dbConnect () : Promise<void> {
+
+    // first check ki databse conneted hai kya?
+
+     if(connetion.isConneted)
+     {
+        console.log("Database is already conneted")
+        return
+     }
+
+     try {
+       const db =  await mongoose.connect(process.env.MONGODB_URI || '')
+       console.log(db.connections)
+       console.log(db)
+       connetion.isConneted = db.connections[0].readyState
+
+       console.log("DB conneted success");
+     } catch (error) {
+
+        console.log("Database connetinon is faild", error)
+        process.exit(1)
+     }
+}
